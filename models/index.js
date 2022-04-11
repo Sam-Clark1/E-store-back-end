c7434fa188dd559a083e903d61fad4395b009146
Sam Clark <samclark2399@gmail.com>
2022-04-11 12:24:55 -0400

    fixed issue in relating models

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ Product.belongsTo(Category, {
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
+  foreignKey: 'product_id'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
